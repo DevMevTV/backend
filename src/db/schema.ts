4ef9dc54c7735642e76f0eed94e32792297aaf4b
Omri H <omrih.e@protonmail.com>
2026-08-03 20:26:42 +0300

    feat: job creation

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,18 @@
-import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgEnum, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const worlds = pgTable("worlds", {
   uuid: uuid().primaryKey(),
   name: varchar().notNull(),
-  token: varchar().notNull(),
+  token: varchar().notNull().unique(),
   verified: boolean().notNull().default(false),
+})
+
+export const jobType = pgEnum("type", ["buy", "sell"])
+
+export const jobs = pgTable("jobs", {
+  id: varchar().primaryKey(),
+  token: varchar().notNull().unique(),
+  type: jobType().notNull(),
+  amount: integer().notNull(),
+  worldToken: varchar("world_token").references(() => worlds.token).notNull(),
 })
--- src/routes/job/index.ts
+++ src/routes/job/index.ts
@@ -1,21 +1,23 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import { db } from "../../index.js";
-import { worlds } from "../../db/schema.js";
+import { jobs, worlds } from "../../db/schema.js";
 import { DrizzleError, DrizzleQueryError, eq } from "drizzle-orm";
 import { generateToken } from "../../util.js";
 import { DatabaseError } from "pg";
 
-type CreateWorldBody = {
-  uuid: string;
-  name: string;
+type CreateJobBody = {
+  id: string;
+  type: "buy" | "sell";
+  amount: number;
+  world_token: string;
 };
 
-type CreateWorldResponse = {
+type CreateJobResponse = {
   success: true;
   token: string;
 };
 
-type CreateWorldErorResponse = {
+type CreateJobErrorResponse = {
   success: false;
   error: string;
 };
@@ -25,16 +27,32 @@ export default async function (
   opts: FastifyPluginOptions,
 ) {
   fastify.post<{
-    Body: CreateWorldBody;
-    Reply: CreateWorldResponse | CreateWorldErorResponse;
+    Body: CreateJobBody;
+    Reply: CreateJobResponse | CreateJobErrorResponse;
   }>("/create", async (request, reply) => {
-    const { uuid, name } = request.body;
+    const { id, world_token, type, amount } = request.body;
+    try {
+      const world = (
+        await db.select().from(worlds).where(eq(worlds.token, world_token))
+      )[0];
+      if (!world.verified) {
+        return reply
+          .status(400)
+          .send({ success: false, error: `World not verified` });
+      }
+    } catch (err) {
+      return reply
+        .status(400)
+        .send({ success: false, error: `World not found` });
+    }
     const token = generateToken();
     try {
-      await db.insert(worlds).values({
-        uuid: uuid,
-        name: name,
+      await db.insert(jobs).values({
+        id: id,
         token: token,
+        type: type,
+        amount: amount,
+        worldToken: world_token,
       });
     } catch (err) {
       if (err instanceof DrizzleQueryError) {
